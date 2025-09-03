e2041e0b91c47af2fcca583f372c59e9a6b3a975
sebascastellanos <sebastian.castellanos2012@gmail.com>
2025-09-03 14:42:06 -0500

    feat/nuevoscambios

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+# main.py (en la raíz del repo)
+import os, sys
 
+# agrega ./src al sys.path para que "flow", "tasks", etc. sean importables
+HERE = os.path.dirname(__file__)
+SRC = os.path.join(HERE, "src")
+if SRC not in sys.path:
+    sys.path.insert(0, SRC)
 
-from ProyectoBigData.src.flow.main_flow import crime_data_ingestion_pipeline
+from flow.etl_flow import etl_clean_flow  # importa tu flow
 
 if __name__ == "__main__":
-    result = crime_data_ingestion_pipeline()
-    print("Pipeline ejecutado exitosamente!")
+    # nombre del CSV que está en data/bronze/
+    etl_clean_flow(csv_file="Crime_Incidents_in_2025.csv")
